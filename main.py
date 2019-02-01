efd5f57b114a829f74a7dbebc002e670adfdb6e9
CalvT <calvin@jstomkins.com>
2019-02-01 12:59:07 -0300

    Corrections from Andras Deak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import re
-from datetime import timedelta
 from datetime import datetime
 import time
 import BotpySE as bp
@@ -11,44 +10,36 @@ password = cbenv.password
 
 commands = bp.all_commands
 
-rooms = [57773]
+bot = bp.Bot("CharlieB", commands, 57773, [], "stackexchange.com", email, password)
 
-bot = bp.Bot("CharlieB", commands, rooms, [], "stackexchange.com", email, password)
-
-global t
-t = datetime.now()
-
-def cbm(cbm):
-  bot.post_global_message('[ [CharlieB](https://github.com/CalvT/CommentSmoker) ] ' + cbm)
+def cbm(msg):
+    bot.post_global_message('[ [CharlieB](https://github.com/CalvT/CommentSmoker) ] ' + msg)
 
 l = []
 def pullcomments():
-  s = datetime.now()
-  regex = r'.*<a href=\"http(|s):\/\/(?!(meta\.|(meta\.|)[a-z]*\.|)(stackoverflow|stackexchange))'
-  comments = requests.get('http://api.stackexchange.com/2.2/comments?page=1&pagesize=75&key=IAkbitmze4B8KpacUfLqkw((&order=desc&sort=creation&site=stackoverflow&filter=!SWK9z*gpvuT.wQS8A.').json() 
-  items = comments['items']
-  a = 0
-  b = 0
-  c = 0
-  for i in items:
-    data = items[a]
-    if data['owner']['reputation'] < 50:
-      if data['comment_id'] in l:
-        c = c + 1
-      else:
-        if re.match(regex, str(data['body'])):
-          msg = '[Link](' + data['link'] + ') | Rep: ' + str(data['owner']['reputation']) + ' | Comment: `' + data['body'] + '`'
-          cbm(msg)
-          b = b + 1
-          l.append(data['comment_id'])
-    a = a + 1
-  cbm(str(datetime.now()) + ' Scanned: ' + str(a) + ' | New Matched: ' + str(b) + ' | Previously seen: ' + str(c) + ' | Quota: ' + str(comments['quota_remaining']))
-  s = datetime.now() - s
-  s = s.total_seconds()
-  s = 60 - s
-  time.sleep(s)
+    s = datetime.now()
+    regex = r'.*<a href=\"http(|s):\/\/(?!(meta\.|(meta\.|)[a-z]*\.|)(stackoverflow|stackexchange))'
+    comments = requests.get('http://api.stackexchange.com/2.2/comments?page=1&pagesize=75&key=IAkbitmze4B8KpacUfLqkw((&order=desc&sort=creation&site=stackoverflow&filter=!SWK9z*gpvuT.wQS8A.').json() 
+    items = comments['items']
+    a = b = c = 0
+    for i in items:
+        data = items[a]
+        if data['owner']['reputation'] < 50:
+            if data['comment_id'] in l:
+                c = c + 1
+            else:
+                if re.match(regex, str(data['body'])):
+                    cbm('[Link]({}) | Rep: {} | Comment: `{}`'.format(data['link'], str(data['owner']['reputation'], data['body'])))
+                    b = b + 1
+                    l.append(data['comment_id'])
+        a = a + 1
+    cbm('{} Scanned: {} | New Matched: {} | Previously seen: {} | Quota: {}'.format(datetime.now(), a, b, c, comments['quota_remaining']))
+    s = datetime.now() - s
+    s = s.total_seconds()
+    s = 60 - s
+    time.sleep(s)
 
 bot.start()
 
 while True:
-  pullcomments()
+    pullcomments()
