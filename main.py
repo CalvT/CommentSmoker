1ed2ec6630dcae2c9312c8a7ddd8b82886d9a10a
CalvT <calvin@jstomkins.com>
2019-02-04 11:32:35 -0300

    Add website whitelist, add keyword blacklist

--- main.py
+++ main.py
@@ -10,7 +10,28 @@ password = cbenv.password
 
 commands = bp.all_commands
 
-bot = bp.Bot("CharlieB", commands, [57773], [], "stackexchange.com", email, password)
+websiteWhitelist = [
+    '(meta\.|(meta\.|)[a-z]*\.|)(stackoverflow|stackexchange)',
+    'i\.stack\.imgur\.com',
+    'developer\.mozilla\.org',
+    'docs\.microsoft\.com',
+    'docs\.python\.org',
+    'issues\.apache\.org',
+    'jsfiddle\.net',
+    'php\.net'
+]
+
+keywordBlacklist = [
+    'fu(c|)k',
+    'shit',
+    'downvote'
+]
+
+websiteRegex = '.*<a href=\"http(|s):\/\/(?!' + '|'.join(websiteWhitelist) + ')'
+
+keywordRegex = '(' + ')|('.join(keywordBlacklist) + ')'
+
+bot = bp.Bot('CharlieB', commands, [57773], [], 'stackexchange.com', email, password)
 
 def cbm(msg):
     bot.post_global_message('[ [CharlieB](https://github.com/CalvT/CommentSmoker) ] ' + msg)
@@ -18,7 +39,6 @@ def cbm(msg):
 l = []
 def pullcomments():
     s = datetime.now()
-    regex = r'.*<a href=\"http(|s):\/\/(?!(meta\.|(meta\.|)[a-z]*\.|)(stackoverflow|stackexchange))'
     comments = requests.get('http://api.stackexchange.com/2.2/comments?page=1&pagesize=75&key=IAkbitmze4B8KpacUfLqkw((&order=desc&sort=creation&site=stackoverflow&filter=!SWK9z*gpvuT.wQS8A.').json() 
     items = comments['items']
     a = b = c = 0
@@ -28,8 +48,12 @@ def pullcomments():
             if data['comment_id'] in l:
                 c = c + 1
             else:
-                if re.match(regex, str(data['body'])):
-                    cbm('URL Detected | [Comment]({}): `{}`'.format(data['link'], data['body']))
+                if re.match(websiteRegex, data['body']):
+                    cbm('Website Detected | [Comment]({}): `{}`'.format(data['link'], data['body']))
+                    b = b + 1
+                    l.append(data['comment_id'])
+                elif re.match(keywordRegex, data['body']):
+                    cbm('Keyword Detected | [Comment]({}): `{}`'.format(data['link'], data['body']))
                     b = b + 1
                     l.append(data['comment_id'])
         a = a + 1
