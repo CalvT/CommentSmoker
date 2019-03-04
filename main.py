e1038d97c377e552f4fb05b46eb4ebf925c43f2e
CalvT <calvin@jstomkins.com>
2019-03-04 10:55:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ chqWd = {}
 for line in chqWatched.splitlines():
       when, who, what = line.split('\t', 3)
       chqWd[what] = "when: {0} who: {1}".format(when, who)
-chqWR = r'(?is)(?:^|\b|(?w:\b))(?:{})(?:\b|(?w:\b)|$)'.format('|'.join(chqWd.keys()))
+chqWR = r'(?is)(?:^|\b|(?w:\b))(?:{})'.format('|'.join(chqWd.keys()))
 
 chqDomains = requests.get(chqGH +
                           'blacklisted_websites.txt').text.splitlines()
